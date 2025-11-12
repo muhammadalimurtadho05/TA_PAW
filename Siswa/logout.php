f6658fc4a4d465b0cdd5f202aec3a07c163c2dcf
Achmad Junaidi S <acjusy@gmail.com>
2025-11-12 15:08:36 +0700

    pendaftaran

--- Siswa/logout.php
+++ Siswa/logout.php
@@ -3,8 +3,8 @@ session_start();
 session_unset();
 session_destroy();
 header("Location: ../index.php");
-<<<<<<< HEAD
+
 exit;
-=======
+
 exit;
->>>>>>> 9fd9ece5f364cb5d98ab87b982eb95b47d7954e7
+
--- Siswa/update_profil.php
+++ Siswa/update_profil.php
@@ -59,8 +59,8 @@ $_SESSION['pesan'] = [
 ];
 
 header("Location: index.php");
-<<<<<<< HEAD
+
 exit;
-=======
+
 exit;
->>>>>>> 9fd9ece5f364cb5d98ab87b982eb95b47d7954e7
+
